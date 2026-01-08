ebc21f99d33cf5b9d310a508219d66a7ce98b58b
Shuflduf <shuflduf@gmail.com>
2026-01-07 21:53:38 -0700

    I THINK STAIRS WORK

--- stair_handler.gd
+++ stair_handler.gd
@@ -1,30 +1,27 @@
 extends Node3D
 
-@onready var stair_cast: ShapeCast3D = $StairCast
-@onready var stair_checker: RayCast3D = $StairChecker
 @onready var player: CharacterBody3D = get_parent()
 
 var last_player_vel = Vector3.ZERO
+var rays: Array[CollisionShape3D]
 
-func _physics_process(_delta: float) -> void:
-	if stair_cast.is_colliding():
+func _ready() -> void:
+	for ray in get_children():
+		rays.append(ray)
 		
-		var flat_player_vel = Vector3(player.velocity.x, 0.0, player.velocity.z)
-		if !flat_player_vel.is_zero_approx():
-			last_player_vel = flat_player_vel
-
-		var local_col_point = self.to_local(stair_cast.get_collision_point(0))
-		stair_checker.position.x = local_col_point.x * 1.2
-		stair_checker.position.z = local_col_point.z * 1.2
-		stair_checker.force_raycast_update()
-		var d = stair_checker.position.normalized().dot(last_player_vel.normalized())
-		print(d)
-		if stair_checker.is_colliding() && (flat_player_vel.length() > 2.0 or !player.wish_dir.is_zero_approx()):
-			player.position.y += self.to_local(stair_checker.get_collision_point()).y + 1.01
-			#player.velocity = before_stair_velocity
-			#player.position += player.wish_dir * 0.2
-			#player.velocity = velocity_buffer + (Vector3.UP * 0.5)
-			#player.position += last_wish_dir * 0.2
-			#player.position.x += player.velocity.x * delta * 10.0
-			#player.position.z += player.velocity.z * delta * 10.0
+		ray.reparent.call_deferred(player)
+		
+func _physics_process(delta: float) -> void:
+	pass
+	for ray in rays:
+		var dir = flatten_vec(ray.position).normalized()
+		var player_delta = player.velocity / 10.0
+		player_delta = Vector3(player_delta.x, 0.0, player_delta.z)
+		var ray_pos = dir * (1 + player_delta.length()) * 0.5
+		ray.position.x = ray_pos.x
+		ray.position.z = ray_pos.z
+		print(ray.position)
 			
+
+func flatten_vec(input: Vector3) -> Vector3:
+	return Vector3(input.x, 0.0, input.z)
