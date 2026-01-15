fb0e76f2d81b43e1b21d2cffa350eb17ae7f3272
Shuflduf <shuflduf@gmail.com>
2026-01-14 22:40:49 -0700

    GOOD cleaver throwing

--- Skills/Chef/dice.gd
+++ Skills/Chef/dice.gd
@@ -3,18 +3,24 @@ extends Skill
 @export var projectile: PackedScene
 
 var current_cooldown = 0.0
+var active_cleavers: Array[Node3D]
 
 @onready var cam: Camera3D = get_parent().cam
 
 func use():
+	for cleaver in active_cleavers:
+		cleaver.stay()
+	
 	if current_cooldown > 0.0:
 		return
 		
 	var new_cleaver: Node3D = projectile.instantiate()
 	new_cleaver.target_position = cam.global_position + (-cam.global_transform.basis.z * 15.0)
+	new_cleaver.player = cam
+	new_cleaver.delete.connect(func(): active_cleavers.erase(new_cleaver))
 	get_tree().root.add_child(new_cleaver)
 	new_cleaver.global_position = cam.global_position
-	
+	active_cleavers.append(new_cleaver)
 	
 	current_cooldown = info.cooldown
 
--- Skills/Chef/dice_projectile.gd
+++ Skills/Chef/dice_projectile.gd
@@ -1,10 +1,41 @@
 extends Node3D
 
-var target_position: Vector3
+signal delete
 
-func _ready() -> void:
-	var tween = get_tree().create_tween()
-	tween.tween_property(self, ^"global_position", target_position, 1.0)
+@export var speed = 20.0
+
+enum State {
+	Going,
+	Staying,
+	Back,
+}
+
+var target_position: Vector3
+var held_down = true
+var player: Node3D
+var state: State = State.Going
+
+
+func stay():
+	held_down = true
 
 func _physics_process(delta: float) -> void:
 	rotation.y += delta * 30.0
+	
+	match state:
+		State.Going:
+			global_position += global_position.direction_to(target_position) * delta * speed
+			if global_position.distance_to(target_position) < 0.1:
+				state = State.Staying
+		State.Staying:
+			if !held_down:
+				state = State.Back
+		State.Back:
+			global_position += global_position.direction_to(player.global_position) * delta * speed
+			if global_position.distance_to(player.global_position) < 0.5:
+				queue_free()
+				delete.emit()
+	
+	if held_down: 
+		held_down = false
+	
