9b78f9d3701d6fcb09cdafadd0f8d68451cefc28
Shuflduf <shuflduf@gmail.com>
2026-04-22 09:26:50 -0600

    mouse fix

--- Camera/mouse_system.gd
+++ Camera/mouse_system.gd
@@ -26,7 +26,7 @@ func _unhandled_key_input(event: InputEvent) -> void:
 
 
 func _input(event: InputEvent) -> void:
-	if event is InputEventMouseMotion:
+	if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
 		var mouse_movement = -event.screen_relative * actual_mouse_sens
 		player.rotation.y += mouse_movement.x
 		rotation_offset.x += mouse_movement.y
