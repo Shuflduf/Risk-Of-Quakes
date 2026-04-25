4968c7af4322bd50f3899b5a7996ea0a0ab94c59
Shuflduf <shuflduf@gmail.com>
2026-04-25 00:45:22 -0600

    weapon sway

--- Skills/follow_camera.gd
+++ Skills/follow_camera.gd
@@ -6,5 +6,7 @@ extends Node
 @onready var cam: Camera3D = weapon.get_parent().cam
 
 
-func _process(_delta: float) -> void:
-	weapon.transform = cam.transform.translated_local(offset)
+func _process(delta: float) -> void:
+	#weapon.transform = cam.transform.translated_local(offset)
+	weapon.position = weapon.position.lerp(cam.transform.translated_local(offset).origin, delta * 20.0)
+	weapon.rotation = weapon.rotation.lerp(cam.rotation, delta * 20.0)
