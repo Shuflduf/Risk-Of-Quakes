053d851ac44d5cfab620ec4f234e8fbbe156f92e
Shuflduf <shuflduf@gmail.com>
2026-04-25 01:59:48 -0600

    skill info when hovering

--- UI/HUD/hud.gd
+++ UI/HUD/hud.gd
@@ -10,8 +10,20 @@ var registered_skills: Dictionary[Skill.SkillSlot, Node]
 @onready var death_container: VBoxContainer = %DeathContainer
 @onready var death_message: Label = %DeathMessage
 @onready var respawn_label: Label = %RespawnLabel
+@onready var skill_info_container: PanelContainer = %SkillInfoContainer
+@onready var skill_title: Label = %SkillTitle
+@onready var skill_description: Label = %SkillDescription
 
 var remaining_respawn_time = 0.0
+
+func show_skill_info(skill_info: SkillInfo):
+	print(skill_info.skill_name)
+	skill_info_container.show()
+	skill_title.text = skill_info.skill_name
+	skill_description.text = skill_info.description
+	
+func hide_skill_info():
+	skill_info_container.hide()
 
 func respawn(respawn_seconds: float):
 	if death_container.visible:
@@ -35,6 +47,9 @@ func add_skill(skill_info: SkillInfo, slot: Skill.SkillSlot):
 	skills_container.add_child(new_skill_ui)
 	registered_skills[slot] = new_skill_ui
 	new_skill_ui.create(skill_info)
+	new_skill_ui.mouse_entered.connect(show_skill_info.bind(skill_info))
+	new_skill_ui.mouse_exited.connect(hide_skill_info)
+
 	#prints(skill_info.skill_name, slot)
 
 
