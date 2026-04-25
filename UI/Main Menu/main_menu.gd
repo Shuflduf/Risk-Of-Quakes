249f545baf1fd7e863078393e7ad8f42eaa8d873
Shuflduf <shuflduf@gmail.com>
2026-04-24 23:53:49 -0600

    error message when failing to make lobby

--- UI/Main Menu/main_menu.gd
+++ UI/Main Menu/main_menu.gd
@@ -1,8 +1,9 @@
 extends Control
 
-@onready var username: LineEdit = $Connection/Username
+@onready var username: LineEdit = %Username
 @onready var connection: VBoxContainer = $Connection
 @onready var lobby_panel: HBoxContainer = $Lobby
+@onready var error_label: Label = %ErrorLabel
 
 
 func _ready() -> void:
@@ -11,7 +12,11 @@ func _ready() -> void:
 
 func _on_host_pressed() -> void:
 	Lobby.player_info["name"] = username.text
-	Lobby.create_game()
+	var err = Lobby.create_game()
+	if err:
+		if err == Error.ERR_CANT_CREATE:
+			error_label.text = "Lobby already created on this network!"
+		return
 
 	transition_to_lobby()
 
