4f165a84324e8a7bdb4ec6493bd61cc7352cc645
Shuflduf <shuflduf@gmail.com>
2026-04-27 13:04:19 -0600

    window mode setting

--- Game/settings.gd
+++ Game/settings.gd
@@ -13,7 +13,15 @@ var fov = 110.0:
 		fov = new_val
 		fov_updated.emit(new_val)
 		save_settings()
-
+var window_mode: DisplayServer.WindowMode = DisplayServer.WindowMode.WINDOW_MODE_WINDOWED:
+	set(new_val):
+		window_mode = [
+			DisplayServer.WindowMode.WINDOW_MODE_WINDOWED,
+			DisplayServer.WindowMode.WINDOW_MODE_EXCLUSIVE_FULLSCREEN,
+			DisplayServer.WindowMode.WINDOW_MODE_FULLSCREEN,
+		][new_val]
+		DisplayServer.window_set_mode(window_mode)
+		save_settings()
 
 func _ready() -> void:
 	var settings_file_content = FileAccess.get_file_as_string(SETTINGS_PATH)
@@ -21,8 +29,9 @@ func _ready() -> void:
 		var saved_settings = JSON.parse_string(settings_file_content)
 		sensitivity = saved_settings["sensitivity"]
 		fov = saved_settings["fov"]
+		window_mode = saved_settings["window_mode"]
 
 
 func save_settings():
 	var file = FileAccess.open(SETTINGS_PATH, FileAccess.WRITE)
-	file.store_string(JSON.stringify({"fov": fov, "sensitivity": sensitivity}))
+	file.store_string(JSON.stringify({"fov": fov, "sensitivity": sensitivity, "window_mode": window_mode}))
--- UI/HUD/settings_menu.gd
+++ UI/HUD/settings_menu.gd
@@ -4,11 +4,16 @@ extends MarginContainer
 @onready var sens_spin_box: SpinBox = %SensSpinBox
 @onready var fov_slider: HSlider = %FOVSlider
 @onready var fov_spin_box: SpinBox = %FOVSpinBox
+@onready var window_mode_option: OptionButton = %WindowModeOption
 
 @onready var pairs = {
 	&"sensitivity": [sens_slider, sens_spin_box],
 	&"fov": [fov_slider, fov_spin_box],
 }
+@onready var options = {
+	&"window_mode": window_mode_option,
+}
+
 
 #func _process(delta: float) -> void:
 #if visible:
@@ -29,3 +34,9 @@ func _ready() -> void:
 				setting_controllers[0].value = value
 				Settings.set(setting, value)
 		)
+	for setting in options:
+		var setting_controller = options[setting]
+		setting_controller.selected = Settings.get(setting)
+		setting_controller.item_selected.connect(func(index: int): 
+			print(index)
+			Settings.set(setting, index))
