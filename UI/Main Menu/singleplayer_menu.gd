8686583ebf0fa3c30dff294f190d488e552f5dea
Shuflduf <shuflduf@gmail.com>
2026-04-28 01:25:06 -0600

    start implementing singleplayer

---
+++ UI/Main Menu/singleplayer_menu.gd
@@ -0,0 +1,17 @@
+extends HBoxContainer
+
+
+func _on_start_game_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_go_back_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_map_option_item_selected(index: int) -> void:
+	pass # Replace with function body.
+
+
+func _on_survivor_list_survivor_selected(survivor: String) -> void:
+	pass # Replace with function body.
